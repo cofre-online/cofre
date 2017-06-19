70ebd8a934715170e755d4dd4910b35b5ebea136
Diego Marques <dshwm11@gmail.com>
2017-06-19 08:37:36 -0300

    More tests for Secret Sharing

--- cofre/secret.py
+++ cofre/secret.py
@@ -12,26 +12,23 @@ class SecretSharing(object):
             than the number of shares")
         self.num_shares = num_shares
         self.threshold = threshold
-        self.prime = number.getPrime(256)
 
     def split_secret(self, secret):
         coefficients = []
         coefficients.append(secret)
 
-        for i in range(1, self.threshold-1):
-            coefficients.append(random.randrange(self.prime))
+        prime = number.getPrime(256)
 
-        shares = self._get_points(self.num_shares, coefficients)
+        for i in range(1, self.threshold):
+            coefficients.append(random.randrange(prime))
+
+        shares = self._get_points(self.num_shares, coefficients, prime)
 
         return shares
 
     def reconstruct_secret(self, shares):
-        share_set = set(shares)
-        if (len(share_set) < len(shares)):
-            raise ValueError('provided duplicate share')
-        if (len(share_set) < self.threshold):
-            raise ValueError('insufficient shares')
-        
+        share_set = self._share_set(shares)
+        prime = share_set[0][2]
         secret = 0
         
         for j in range(self.threshold):
@@ -41,25 +38,48 @@ class SecretSharing(object):
             for i in range(self.threshold):
                 if j == i:
                     continue
-                numerator = (numerator * shares[i][0] * -1) % self.prime
-                denominator = (denominator * (shares[j][0] - shares[i][0])) % self.prime
+                numerator = (numerator * shares[i][0] * -1) % prime
+                denominator = (denominator * (shares[j][0] - shares[i][0])) % prime
             
-            secret = (self.prime + secret + (y * numerator * number.inverse(denominator, self.prime))) % self.prime
+            secret = (prime + secret + (y * numerator * number.inverse(denominator, prime))) % prime
 
         return secret
 
     def generate_new_share(self, shares, num):
-        # TODO Recover polynomial f(x) from shares and calculate f(num)
-        pass
+        share_set = self._share_set(shares)
+        prime = share_set[0][2]
 
-    def _get_points(self, n, coefficients):
+        coefficients = self._recover_polynomial(share_set)
+
+        return (num, self._get_point(num, coefficients, prime))
+
+    def _share_set(self, shares):
+        share_set = set(shares)
+        if (len(share_set) < len(shares)):
+            raise ValueError('provided duplicate share')
+        if (len(share_set) < self.threshold):
+            raise ValueError('insufficient shares')
+
+        return share_set
+
+
+    def _get_points(self, n, coefficients, prime):
         points = []
         for i in range(n):
             x = i + 1
-            fx = 0
-            for j in range(len(coefficients)):
-                fx += coefficients[j] * pow(x, j, self.prime)
-
-            points.append((x, fx))
+            fx = self._get_point(x, coefficients)
+            points.append((x, fx, prime))
 
         return points
+
+    def _get_point(self, x, coefficients, prime):
+        fx = 0
+        for j in range(len(coefficients)):
+            fx += coefficients[j] * pow(x, j, prime)
+        
+        return fx
+
+    def _recover_polynomial(self, shares):
+        prime = shares[0][2]
+
+        for i in range(self.threshold):
--- cofre/test/secret_test.py
+++ cofre/test/secret_test.py
@@ -2,20 +2,72 @@ import unittest
 from cofre.secret import SecretSharing
 
 class SecretSharingTestCase(unittest.TestCase):
+    def test_instantiation(self):
+        with self.assertRaises(ValueError):
+            SecretSharing(4, 8)
+
+        with self.assertRaises(ValueError):
+            SecretSharing(-5, 3)
+
+        with self.assertRaises(ValueError):
+            SecretSharing(2, -3)
+
     def test_split_secret(self):
-        secret = 25
+        secret = 10
         ss = SecretSharing(4, 2)
         shares = ss.split_secret(secret)
 
         self.assertEqual(len(shares), 4)
-        with self.assertRaises(ValueError):
-            ss = SecretSharing(4, 8)
+        self.assertEqual(shares[0][0], 1)
+        self.assertEqual(shares[3][0], 4)
 
-        with self.assertRaises(ValueError):
-            ss = SecretSharing(-5, 3)
+        self.assertNotEqual(shares[0][1], secret)
+        self.assertNotEqual(shares[0][1], shares[1][1])
+
+    def test_reconstruct_secret(self):
+        secret = 68468
+        ss = SecretSharing(8, 5)
+        shares = ss.split_secret(secret)
+
+        recon = ss.reconstruct_secret(shares[:5])
+
+        self.assertEqual(recon, secret)
+
+        recon = ss.reconstruct_secret(shares)
+
+        self.assertEqual(recon, secret)
         
         with self.assertRaises(ValueError):
-            ss = SecretSharing(2, -3)
+            ss.reconstruct_secret(shares[0])
+
+        with self.assertRaises(ValueError):
+            ss.reconstruct_secret([shares[0], shares[0], shares[1], shares[2], shares[3]])
+
+    def test_generate_new_share(self):
+        secret = 77545
+        ss = SecretSharing(4, 2)
+        shares = ss.split_secret(secret)
+
+        new_share = ss.generate_new_share(shares, 37)
+        self.assertEqual(new_share[0], 37)
+
+        recon = ss.reconstruct_secret([shares[0], new_share])
+        self.assertEqual(recon, secret)
+
+        new_share = ss.generate_new_share(shares, 2)
+        self.assertEqual(new_share, shares[1])
+
+        with self.assertRaises(ValueError):
+            ss.generate_new_share([shares[0]], 45)
+
+        with self.assertRaises(ValueError):
+            ss.generate_new_share([shares[0], shares[0]], 12)
+
+        with self.assertRaises(ValueError):
+            ss.generate_new_share(shares, 0)
+
+        with self.assertRaises(ValueError):
+            ss.generate_new_share(shares, -5)
 
 if __name__ == '__main__':
     unittest.main()
